a0d1f51cde5104128f3fd16e229007e47597ed17
Seman Kim <sdoramkim@gmail.com>
2023-03-15 15:23:45 +0900

    update app.py
    time 함수 추가
    글 작성시간 time으로호출

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ client = MongoClient('mongodb+srv://sparta:sparta@cluster0.ahs6lya.mongodb.net/?
 db = client.dbsparta
 
 from flask import Flask, render_template, request, jsonify
+import requests
+from bs4 import BeautifulSoup
+import time
 
 app = Flask(__name__)
 
@@ -22,18 +25,44 @@ def home():
 def content3():
     return render_template('record.html')
 
-
-@app.route("/ruff_info", methods=["POST"])
-def _post():
-    name_receive = request.form['name_give']
+@app.route("/books", methods=["POST"])
+def movie_post():
     url_receive = request.form['url_give']
+    star_receive = request.form['star_give']
+    reviewer_receive = request.form['reviewer_give']
+    comment_receive = request.form['comment_give']
+
+    data = requests.get(url_receive)
+    soup = BeautifulSoup(data.text, "html.parser")
+
+    img_tag = soup.select_one("img[src^='https://contents.kyobobook.co.kr/sih/fit-in/']")
+    img_url = img_tag.get("src")
+
+    title_tag = soup.select_one(
+        "#contents > div.prod_detail_header > div > div.prod_detail_title_wrap > div > div.prod_title_box.auto_overflow_wrap > div.auto_overflow_contents > div > h1 > span")
+    title_name = title_tag.text
+
+    author_tag = soup.select_one(
+        "#contents > div.prod_detail_header > div > div.prod_detail_view_wrap > div.prod_detail_view_area > div:nth-child(1) > div > div.prod_author_box.auto_overflow_wrap > div.auto_overflow_contents > div > div > a:nth-child(1)")
+    author_name = author_tag.text
 
     doc = {
-        'name': name_receive,
-        'url': url_receive,
+        'image': img_url,
+        'title': title_name,
+        'author': author_name,
+        'star': star_receive,
+        'reviewer': reviewer_receive,
+        'comment': comment_receive
     }
-    db.ruff_info.insert_one(doc)
-    return jsonify({'msg': '주문 완료!'})
+
+    db.books.insert_one(doc)
+    return jsonify({'msg': '저장 완료!'})
+
+
+@app.route("/books", methods=["GET"])
+def movie_get():
+    book_list = list(db.books.find({}, {'_id': False}))
+    return jsonify({'books': book_list})
 
 
 @app.route("/members", methods=["POST"])
@@ -43,13 +72,15 @@ def members_post():
     mbti_receive = request.form['mbti_give']
     book_receive = request.form['book_give']
     blog_receive = request.form['blog_give']
+    time_receive = (time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
 
     doc = {
         'image': image_receive,
         'name': name_receive,
         'mbti': mbti_receive,
         'book': book_receive,
-        'blog': blog_receive
+        'blog': blog_receive,
+        'time': time_receive
     }
     db.members.insert_one(doc)
     return jsonify({'msg': '작성 완료!'})
